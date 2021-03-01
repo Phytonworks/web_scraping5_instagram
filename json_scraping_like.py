229d93d68983ae615e0ae9cd59bf86943991bbd3
Eka Bernard <bernard.eka@gmail.com>
2021-03-01 23:20:19 +0700

    Instagram crapping using request & json

--- json_scraping_like.py
+++ json_scraping_like.py
@@ -1,19 +1,12 @@
-
 import requests
-import json
 import json
 
 url = 'https://www.instagram.com/graphql/query'
-variable = {"id":"1612977390","include_reel":True,"fetch_mutual":True,"first":24}
+variable = {"shortcode": "CLygOc1MFqh", "include_reel": True, "first": 24}
 
 params = {
-    'query_hash: 5aefa9893005572d237da5068082d8d5',
-    'variables: json.dumps(variable)'
+    'query_hash': 'd5d763b1e2acf209d62d22d184488e57',
+    'variables': json.dumps(variable)
 }
-
-r = requests.get(url,params=params).json()
-
-users = r{['data']['shortcode_media']['edge_liked_by']['edges']}
-
-for user in users:
-    print(user)
+r = requests.get(url, params=params).json
+print(r)
